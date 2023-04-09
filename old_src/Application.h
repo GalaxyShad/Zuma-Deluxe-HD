eff1cef2ac3fb3c401c11622356b63a4344477a0
GalaxyShad <vadim.kadyrin@mail.ru>
2023-04-09 19:23:55 +0500

    misc: removed old_src from repo

--- old_src/Application.h
+++
@@ -1,6 +0,0 @@
-#ifndef APPLICATION_H
-#define APPLICATION_H
-
-int ApplicationZuma_Start(void);
-
-#endif 
--- old_src/main.c
+++
@@ -1,5 +0,0 @@
-#include "Application.h"
-
-int main(int argc, char ** args) { 
-    return ApplicationZuma_Start();
-}
